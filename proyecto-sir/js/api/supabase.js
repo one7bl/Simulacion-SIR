4c4b31fa42fd2207024f46c81a6ae690140ff6f9
Onell <130798246+one7bl@users.noreply.github.com>
2026-06-04 20:11:31 -0500

    Delete proyecto-sir directory
    Lo borre

--- proyecto-sir/js/api/supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-
-// Configuración de Supabase
-// REEMPLAZA ESTOS VALORES CON LOS DE TU PROYECTO
-export const SUPABASE_URL = 'https://dntysilkoatmbjbttvtn.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRudHlzaWxrb2F0bWJqYnR0dnRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MDgzOTksImV4cCI6MjA5NjE4NDM5OX0.TmDXEE9f6UVfgbsKYNztI_S6wwZRqaccgbWOh037Pr8';
-
-// Inicializar el cliente de Supabase
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
